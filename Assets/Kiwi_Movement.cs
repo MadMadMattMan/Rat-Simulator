b579fd5a0f3ef3822a8044f2af37b78846619fcb
MadMadMattMan <92753107+MadMadMattMan@users.noreply.github.com>
2023-08-02 09:34:24 +1200

    0.006

--- Assets/Kiwi_Movement.cs
+++ Assets/Kiwi_Movement.cs
@@ -5,14 +5,50 @@ using UnityEngine;
 
 public class Kiwi_Movement : MonoBehaviour
 {
-    public float speed = 0.05f;
+    public float speed = 0.0025f;
+    public float XInput;
+    public float YInput;
 
-    // Update is called once per frame
+    private Rigidbody2D rb;
+
+    public Sprite KiwiDown;
+    public Sprite KiwiUp;
+    public Sprite KiwiSide;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
     void Update()
     {
         float inputX = Input.GetAxis("Horizontal");
+        XInput = inputX;
         float inputY = Input.GetAxis("Vertical");
+        YInput = inputY;
 
         transform.position += new Vector3(speed * inputX, speed * inputY);
+        if (inputY < 0)
+        {
+            gameObject.GetComponent<SpriteRenderer>().sprite = KiwiDown;
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (inputY > 0)
+        {
+            gameObject.GetComponent<SpriteRenderer>().sprite = KiwiUp;
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (inputX < 0)
+        {
+            gameObject.GetComponent<SpriteRenderer>().sprite = KiwiSide;
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (inputX > 0)
+        {
+            gameObject.GetComponent<SpriteRenderer>().sprite = KiwiSide;
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+
     }
 }
