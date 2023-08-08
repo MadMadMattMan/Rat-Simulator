3d76dd29b76b3cdbbcd835dd32c68bf7146ca742
MadMadMattMan <92753107+MadMadMattMan@users.noreply.github.com>
2023-08-08 15:14:41 +1200

    0.0075

---
+++ Assets/Shop_Outside_Trigger.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop_Outside_Trigger : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Enter Shop Scene       
+    }
+}
