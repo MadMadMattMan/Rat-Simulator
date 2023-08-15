48f97a05c96d36a6d8adc0eaefd0a6bcd3fdc1c2
MadMadMattMan <92753107+MadMadMattMan@users.noreply.github.com>
2023-08-16 09:32:33 +1200

    0.011

--- Assets/Scripts/Hideout_Script.cs
+++ Assets/Scripts/Hideout_Script.cs
@@ -2,16 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IKEA_StoreScript : MonoBehaviour
+public class Hideout_Script : MonoBehaviour
 {
-    public Collider2D StoreTP;
-    public Transform StoreTPMainMap;
+    public Transform HideoutTPMainMap;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.name == "Player")
         {
-            collision.gameObject.GetComponent<Transform>().position = StoreTPMainMap.position;
+            collision.gameObject.GetComponent<Transform>().position = HideoutTPMainMap.position;
         }
     }
 }
