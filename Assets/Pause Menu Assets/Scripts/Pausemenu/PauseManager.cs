591f1763337fba9e5d430e6ccbc71808c4d281e1
MadMadMattMan <92753107+MadMadMattMan@users.noreply.github.com>
2023-08-17 10:21:09 +1200

    0.012

---
+++ Assets/Pause Menu Assets/Scripts/Pausemenu/PauseManager.cs
@@ -0,0 +1,527 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.IO;
+//using UnityStandardAssets.ImageEffects;
+/// <summary>
+///  Copyright (c) 2016 Eric Zhu 
+/// </summary>
+namespace GreatArcStudios
+{
+    /// <summary>
+    /// The pause menu manager. You can extend this to make your own. Everything is pretty modular, so creating you own based off of this should be easy. Thanks for downloading and good luck! 
+    /// </summary>
+    public class PauseManager : MonoBehaviour
+    {
+        /// <summary>
+        /// This is the main panel holder, which holds the main panel and should be called "main panel"
+        /// </summary> 
+        public GameObject mainPanel;
+        /// <summary>
+        /// This is the audio panel holder, which holds all of the silders for the audio panel and should be called "audio panel"
+        /// </summary>
+        public GameObject audioPanel;
+        /// <summary>
+        /// These are the game objects with the title texts like "Pause menu" and "Game Title" 
+        /// </summary>
+        public GameObject TitleTexts;
+        /// <summary>
+        /// The mask that makes the scene darker  
+        /// </summary>
+        public GameObject mask;
+        /// <summary>
+        /// Audio Panel animator
+        /// </summary>
+        public Animator audioPanelAnimator;
+        /// <summary>
+        /// Quit Panel animator  
+        /// </summary>
+        public Animator quitPanelAnimator;
+        /// <summary>
+        /// Pause menu text 
+        /// </summary>
+        public Text pauseMenu;
+
+        /// <summary>
+        /// Main menu level string used for loading the main menu. This means you'll need to type in the editor text box, the name of the main menu level, ie: "mainmenu";
+        /// </summary>
+        public String mainMenu;
+        //DOF script name
+        /// <summary>
+        /// The main camera, assign this through the editor. 
+        /// </summary>        
+        public Camera mainCam;
+        internal static Camera mainCamShared;
+        /// <summary>
+        /// The main camera game object, assign this through the editor. 
+        /// </summary> 
+        public GameObject mainCamObj;
+
+        /// <summary>
+        /// Timescale value. The defualt is 1 for most games. You may want to change it if you are pausing the game in a slow motion situation 
+        /// </summary> 
+        public float timeScale = 1f;
+
+        public Slider audioMasterSlider;
+        public Slider audioMusicSlider;
+        public Slider audioEffectsSlider;
+        /// <summary>
+        /// Inital shadow distance 
+        /// <summary>
+        /// Inital vsync count, the Unity docs say,
+        /// <code> 
+        /// //This will set the game to have one VSync per frame
+        /// QualitySettings.vSyncCount = 1;
+        /// </code>
+        /// <code>
+        /// //This will disable vsync
+        /// QualitySettings.vSyncCount = 0;
+        /// </code>
+        /// </summary>        /// <summary>
+        /// The preset text label.
+        /// </summary>
+        public Text presetLabel;
+        /// <summary>
+        /// Resolution text label.
+        /// </summary>
+        /// <summary>
+        /// Lod bias float array. You should manually assign these based on the quality level.
+        /// </summary>
+        public float[] LODBias;
+        /// <summary>
+        /// Shadow distance array. You should manually assign these based on the quality level.
+        /// </summary>
+        public float[] shadowDist;
+        /// <summary>
+        /// An array of music audio sources
+        /// </summary>
+        public AudioSource[] music;
+        /// <summary>
+        /// An array of sound effect audio sources
+        /// </summary>
+        public AudioSource[] effects;
+        /// <summary>
+        /// An array of the other UI elements, which is used for disabling the other elements when the game is paused.
+        /// </summary>
+        public GameObject[] otherUIElements;
+        /// <summary>
+        /// Editor boolean for hardcoding certain video settings. It will allow you to use the values defined in LOD Bias and Shadow Distance
+        /// </summary>
+        public Boolean hardCodeSomeVideoSettings;
+        /// <summary>
+        /// Boolean for turning on simple terrain
+        /// </summary>
+        public Boolean useSimpleTerrain;
+        public static Boolean readUseSimpleTerrain;
+        /// <summary>
+        /// Event system
+        /// </summary>
+        public EventSystem uiEventSystem;
+        /// <summary>
+        /// Defualt selected on the video panel
+        /// <summary>
+        /// Defualt selected on the video panel
+        /// </summary>
+        public GameObject defualtSelectedAudio;
+        /// <summary>
+        /// Defualt selected on the video panel
+        /// </summary>
+        public GameObject defualtSelectedMain;
+        //last music multiplier; this should be a value between 0-1
+        internal static float lastMusicMult;
+        //last audio multiplier; this should be a value between 0-1
+        internal static float lastAudioMult;
+        //Initial master volume
+        internal static float beforeMaster;
+        //last texture limit 
+        internal static int lastTexLimit;
+        //int for amount of effects
+        private int _audioEffectAmt = 0;
+        //Inital audio effect volumes
+        private float[] _beforeEffectVol;
+
+        //Initial music volume
+        private float _beforeMusic;
+        //Preset level
+        private int _currentLevel;
+        //Resoutions
+        private Resolution[] allRes;
+        //Camera dof script
+        private MonoBehaviour tempScript;
+        //Presets 
+        private String[] presets;
+        //Fullscreen Boolean
+        private Boolean isFullscreen;
+        //current resoultion
+        internal static Resolution currentRes;
+        //Last resoultion 
+        private Resolution beforeRes;
+
+        //last shadow cascade value
+        internal static int lastShadowCascade;
+
+        public static Boolean aoBool;
+        public static Boolean dofBool;
+        private Boolean lastAOBool;
+        private Boolean lastDOFBool;
+        public static Terrain readTerrain;
+        public static Terrain readSimpleTerrain;
+
+        private SaveSettings saveSettings = new SaveSettings();
+        /*
+        //Color fade duration value
+        //public float crossFadeDuration;
+        //custom color
+        //public Color _customColor;
+        
+         //Animation clips
+         private AnimationClip audioIn;
+         private AnimationClip audioOut;
+         public AnimationClip vidIn;
+         public AnimationClip vidOut;
+         public AnimationClip mainIn;
+         public AnimationClip mainOut; 
+          */
+        //Blur Variables
+        //Blur Effect Script (using the standard image effects package) 
+        //public Blur blurEffect;
+        //Blur Effect Shader (should be the one that came with the package)
+        //public Shader blurEffectShader;
+        //Boolean for if the blur effect was originally enabled
+        //public Boolean blurBool;
+
+        /// <summary>
+        /// The start method; you will need to place all of your inital value getting/setting here. 
+        /// </summary>
+        /// 
+
+        public bool GamePaused;
+
+        public void Start()
+        {
+            GamePaused = false;
+            mainCamShared = mainCam;
+            //get all specified audio source volumes
+            _beforeEffectVol = new float[_audioEffectAmt];
+            beforeMaster = AudioListener.volume;
+            //enable titles
+            TitleTexts.SetActive(false);
+            //Disable other panels
+            mainPanel.SetActive(false);
+            audioPanel.SetActive(false);
+            //Enable mask
+            mask.SetActive(false);
+            //set last texture limit
+            lastTexLimit = QualitySettings.masterTextureLimit;
+            //set last shadow cascade 
+            lastShadowCascade = QualitySettings.shadowCascades;
+
+            //set the blur boolean to false;
+            //blurBool = false;
+            //Add the blur effect
+            /*mainCamObj.AddComponent(typeof(Blur));
+            blurEffect = (Blur)mainCamObj.GetComponent(typeof(Blur));
+            blurEffect.blurShader = blurEffectShader;
+            blurEffect.enabled = false;  */
+
+        }
+        /// <summary>
+        /// Restart the level by loading the loaded level.
+        /// </summary>
+        public void Restart()
+        {
+            Application.LoadLevel(Application.loadedLevel);
+            uiEventSystem.firstSelectedGameObject = defualtSelectedMain;
+        }
+        /// <summary>
+        /// Method to resume the game, so disable the pause menu and re-enable all other ui elements
+        /// </summary>
+        public void Resume()
+        {
+            Time.timeScale = timeScale;
+
+            mainPanel.SetActive(false);
+            audioPanel.SetActive(false);
+            TitleTexts.SetActive(false);
+            mask.SetActive(false);
+            for (int i = 0; i < otherUIElements.Length; i++)
+            {
+                otherUIElements[i].gameObject.SetActive(true);
+            }
+            /* if (blurBool == false)
+             {
+                 blurEffect.enabled = false;
+             }
+             else
+             {
+                 //if you want to add in your own stuff do so here
+                 return;
+             } */
+        }
+        /// <summary>
+        /// All the methods relating to qutting should be called here.
+        /// </summary>
+        public void quitOptions()
+        {
+            audioPanel.SetActive(false);
+            quitPanelAnimator.enabled = true;
+            quitPanelAnimator.Play("QuitPanelIn");
+
+        }
+        /// <summary>
+        /// Method to quit the game. Call methods such as auto saving before qutting here.
+        /// </summary>
+        public void quitGame()
+        {
+            Application.Quit();
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#endif
+        }
+        /// <summary>
+        /// Cancels quittting by playing an animation.
+        /// </summary>
+        public void quitCancel()
+        {
+            quitPanelAnimator.Play("QuitPanelOut");
+        }
+        /// <summary>
+        ///Loads the main menu scene.
+        /// </summary>
+        public void returnToMenu()
+        {
+            Application.LoadLevel(mainMenu);
+            uiEventSystem.SetSelectedGameObject(defualtSelectedMain);
+        }
+
+        // Update is called once per frame
+        /// <summary>
+        /// The update method. This mainly searches for the user pressing the escape key.
+        /// </summary>
+        public void FixedUpdate()
+        {
+            //colorCrossfade();
+            if (audioPanel.active == true)
+            {
+                pauseMenu.text = "Audio Menu";
+            }
+            else if (mainPanel.active == true)
+            {
+                pauseMenu.text = "Pause Menu";
+            }
+
+            if (Input.GetKeyDown(KeyCode.Escape) && GamePaused == false)
+            {
+                GamePaused = true;
+                audioPanel.SetActive(false);
+                TitleTexts.SetActive(true);
+                mask.SetActive(true);
+                Time.timeScale = 0;
+                for (int i = 0; i < otherUIElements.Length; i++)
+                {
+                    otherUIElements[i].gameObject.SetActive(false);
+                }
+                /* if (blurBool == false)
+                  {
+                     blurEffect.enabled = true;
+                 }  */
+            }
+            else if (Input.GetKeyDown(KeyCode.Escape) && GamePaused == true) {
+                GamePaused = false;
+                Time.timeScale = timeScale;
+                mainPanel.SetActive(false);
+                audioPanel.SetActive(false);
+                TitleTexts.SetActive(false);
+                mask.SetActive(false);
+                for (int i = 0; i < otherUIElements.Length; i++)
+                {
+                    otherUIElements[i].gameObject.SetActive(true);
+                }
+            }
+
+
+
+        }
+        /*
+        void colorCrossfade()
+        {
+            Debug.Log(pauseMenu.color);
+
+            if (pauseMenu.color == Color.white)
+            {
+                pauseMenu.CrossFadeColor(_customColor, crossFadeDuration, true, false);
+            }
+            else { 
+                pauseMenu.CrossFadeColor(Color.white, crossFadeDuration, true, false);
+            }
+        }  */
+        /////Audio Options
+
+        /// <summary>
+        /// Show the audio panel 
+        /// </summary>
+        public void Audio()
+        {
+            mainPanel.SetActive(false);
+            audioPanel.SetActive(true);
+            audioPanelAnimator.enabled = true;
+            audioIn();
+            pauseMenu.text = "Audio Menu";
+        }
+        /// <summary>
+        /// Play the "audio panel in" animation.
+        /// </summary>
+        public void audioIn()
+        {
+            uiEventSystem.SetSelectedGameObject(defualtSelectedAudio);
+            audioPanelAnimator.Play("Audio Panel In");
+            audioMasterSlider.value = AudioListener.volume;
+            //Perform modulo to find factor f to allow for non uniform music volumes
+            float a; float b; float f;
+            try
+            {
+                a = music[0].volume;
+                b = music[1].volume;
+                f = a % b;
+                audioMusicSlider.value = f;
+            }
+            catch
+            {
+                Debug.Log("You do not have multiple audio sources");
+                audioMusicSlider.value = lastMusicMult;
+            }
+            //Do this with the effects
+            try
+            {
+                a = effects[0].volume;
+                b = effects[1].volume;
+                f = a % b;
+                audioEffectsSlider.value = f;
+            }
+            catch
+            {
+                Debug.Log("You do not have multiple audio sources");
+                audioEffectsSlider.value = lastAudioMult;
+            }
+
+        }
+        /// <summary>
+        /// Audio Option Methods
+        /// </summary>
+        /// <param name="f"></param>
+        public void updateMasterVol(float f)
+        {
+
+            //Controls volume of all audio listeners 
+            AudioListener.volume = f;
+        }
+        /// <summary>
+        /// Update music effects volume
+        /// </summary>
+        /// <param name="f"></param>
+        public void updateMusicVol(float f)
+        {
+            try
+            {
+                for (int _musicAmt = 0; _musicAmt < music.Length; _musicAmt++)
+                {
+                    music[_musicAmt].volume *= f;
+                }
+            }
+            catch
+            {
+                Debug.Log("Please assign music sources in the manager");
+            }
+            //_beforeMusic = music.volume;
+        }
+        /// <summary>
+        /// Update the audio effects volume
+        /// </summary>
+        /// <param name="f"></param>
+        public void updateEffectsVol(float f)
+        {
+            try
+            {
+                for (_audioEffectAmt = 0; _audioEffectAmt < effects.Length; _audioEffectAmt++)
+                {
+                    //get the values for all effects before the change
+                    _beforeEffectVol[_audioEffectAmt] = effects[_audioEffectAmt].volume;
+
+                    //lower it by a factor of f because we don't want every effect to be set to a uniform volume
+                    effects[_audioEffectAmt].volume *= f;
+                }
+            }
+            catch
+            {
+                Debug.Log("Please assign audio effects sources in the manager.");
+            }
+
+        }
+        /// <summary> 
+        /// The method for changing the applying new audio settings
+        /// </summary>
+        public void applyAudio()
+        {
+            StartCoroutine(applyAudioMain());
+            uiEventSystem.SetSelectedGameObject(defualtSelectedMain);
+
+        }
+        /// <summary>
+        /// Use an IEnumerator to first play the animation and then change the audio settings
+        /// </summary>
+        /// <returns></returns>
+        internal IEnumerator applyAudioMain()
+        {
+            audioPanelAnimator.Play("Audio Panel Out");
+            yield return StartCoroutine(CoroutineUtilities.WaitForRealTime((float)audioPanelAnimator.GetCurrentAnimatorClipInfo(0).Length));
+            mainPanel.SetActive(true);
+            audioPanel.SetActive(false);
+            beforeMaster = AudioListener.volume;
+            lastMusicMult = audioMusicSlider.value;
+            lastAudioMult = audioEffectsSlider.value;
+            saveSettings.SaveGameSettings();
+        }
+        /// <summary>
+        /// Cancel the audio setting changes
+        /// </summary>
+        public void cancelAudio()
+        {
+            uiEventSystem.SetSelectedGameObject(defualtSelectedMain);
+            StartCoroutine(cancelAudioMain());
+        }
+        /// <summary>
+        /// Use an IEnumerator to first play the animation and then change the audio settings
+        /// </summary>
+        /// <returns></returns>
+        internal IEnumerator cancelAudioMain()
+        {
+            audioPanelAnimator.Play("Audio Panel Out");
+            // Debug.Log(audioPanelAnimator.GetCurrentAnimatorClipInfo(0).Length);
+            yield return StartCoroutine(CoroutineUtilities.WaitForRealTime((float)audioPanelAnimator.GetCurrentAnimatorClipInfo(0).Length));
+            mainPanel.SetActive(true);
+            audioPanel.SetActive(false);
+            AudioListener.volume = beforeMaster;
+            //Debug.Log(_beforeMaster + AudioListener.volume);
+            try
+            {
+
+
+                for (_audioEffectAmt = 0; _audioEffectAmt < effects.Length; _audioEffectAmt++)
+                {
+                    //get the values for all effects before the change
+                    effects[_audioEffectAmt].volume = _beforeEffectVol[_audioEffectAmt];
+                }
+                for (int _musicAmt = 0; _musicAmt < music.Length; _musicAmt++)
+                {
+                    music[_musicAmt].volume = _beforeMusic;
+                }
+            }
+            catch
+            {
+                Debug.Log("please assign the audio sources in the manager");
+            }
+        }
+    }
+}
