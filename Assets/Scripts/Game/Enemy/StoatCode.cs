b93780fa1361c5f7744068cf48e3a1a23a8cca29
MadMadMattMan <92753107+MadMadMattMan@users.noreply.github.com>
2023-09-14 10:26:38 +1200

    0.024

--- Assets/Scripts/Game/Enemy/StoatCode.cs
+++ Assets/Scripts/Game/Enemy/StoatCode.cs
@@ -6,11 +6,51 @@ public class StoatCode : MonoBehaviour
 {
     public Transform Player;
 
+    public float StoatRoam = 0.5f;
+    public float StoatChase = 0.75f;
+    public float StoatSprint = 0.95f;
+
+    public float StoatSpeed;
+
     public Vector3 DistanceToPlayer;
+
+    public string StoatState;
 
 
     private void Update()
     {
         DistanceToPlayer = transform.position - Player.position;
+    }
+
+    private void OnTriggerStay2D(Collider2D triggerobject)
+    {
+        if (triggerobject.name == "Player")
+        {
+            StoatState = "Active";
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D triggerobject)
+    {
+        if (triggerobject.name == "Player")
+        {
+            StoatState = "Idle";
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        StartCoroutine(StoatStateMovement());
+    }
+
+    public IEnumerator StoatStateMovement()
+    {
+        while (StoatState == "Active")
+        {
+            transform.position -= DistanceToPlayer.normalized * Time.deltaTime * StoatSpeed;
+        }
+
+        return null;
     }
 }
